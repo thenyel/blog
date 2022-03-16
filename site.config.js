e2962ed0b8aebb7941bb99b3df8ce9596e17aa47
Danniel Condez <ducondez@gmail.com>
2022-03-16 13:29:59 -0700

    update notion page id

--- site.config.js
+++ site.config.js
@@ -1,25 +1,25 @@
 module.exports = {
   // where it all starts -- the site's root Notion page (required)
-  rootNotionPageId: '78fc5a4b88d74b0e824e29407e9f1ec1',
+  rootNotionPageId: '13ea2b65fc67472cb3aeaebda1d96d6b',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Transitive Bullshit',
+  name: 'Danniel Condez Blog',
   domain: 'transitivebullsh.it',
-  author: 'Travis Fischer',
+  author: 'Danniel Condez',
 
   // open graph metadata (optional)
   description: 'Example site description',
-  socialImageTitle: 'Transitive Bullshit',
+  socialImageTitle: 'Danniel Condez Blog',
   socialImageSubtitle: 'Hello World! 👋',
 
   // social usernames (optional)
-  twitter: 'transitive_bs',
-  github: 'transitive-bullshit',
-  linkedin: 'fisch2',
+  twitter: 'nyelcondez',
+  github: 'thenyel',
+  linkedin: 'danniel-condez-89332a32',
 
   // default notion icon and cover images for site-wide consistency (optional)
   // page-specific values will override these site-wide defaults
